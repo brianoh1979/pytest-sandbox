3e77f296460efa9f324406d951bea07e3da1dd06
Brian O'Halloran <brian_o_halloran@hotmail.com>
2023-01-26 17:18:50 +0000

    Adding in more test files to push the test run to be greater than 1 minute

---
+++ tests/0112_basic_test.py
@@ -0,0 +1,18 @@
+from other_code.services import DATA_SET_A, DATA_SET_B, DATA_SET_C
+import pytest
+
+def test_example():
+    """
+    But really, test cases should be callables containing assertions:
+    """
+    print("\nRunning test_example...")
+    assert DATA_SET_A == DATA_SET_B
+
+@pytest.fixture
+def test_with_local_fixture_2(local_fixture):
+    """
+    Fixtures can be invoked simply by having a positional arg
+    with the same name as a fixture:
+    """
+    print("Running test_with_local_fixture...")
+    assert True
